bb714f724a22acda19168b0a7709046224ca3c7a
Anurag Sen <anurag007sen@gmail.com>
2024-02-27 09:06:08 +0530

    Project initialized

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Heckehon_3_Project"
+include(":app")
+ 
